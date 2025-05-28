829787ef46dd23cdc44426dce0c8c2550a749663
manish14iitg <111654062+manish14iitg@users.noreply.github.com>
2025-05-29 00:41:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(cors({
 }));app.use(express.json());
 
 app.use("/api", authRoutes);
+app.get("/", (req,res) => {
+  res.json({message: "backend is running"});
+});
 
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => {
